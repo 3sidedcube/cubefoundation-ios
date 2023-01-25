8b62380f1bdd41988a9d0253f7c67a771c150a8b
Ben Shutt <ben@3sidedcube.com>
2023-01-25 17:51:20 +0000

    Add support for `TextStyle` for `TextField`

--- Sources/CubeFoundationSwiftUI/Text/View+TextStyle.swift
+++ Sources/CubeFoundationSwiftUI/Text/View+TextStyle.swift
@@ -26,6 +26,28 @@ public extension Text {
     }
 }
 
+public extension TextField {
+
+    /// Applies all aspects of a `TextStyle` to this `TextField`.
+    ///
+    /// - Warning:
+    /// `.tracking` and `.underline` are not supported
+    @available(iOS, obsoleted: 16.0, message: "Use `View.style(_ style: TextStyle)`")
+    func style(_ style: TextStyle) -> some View {
+        // Initialise as a UIFont to get the intrinsic line height.
+        let uiFont = UIFont(name: "\(style.font.string) \(style.weight.name)", size: style.size)
+        let fontLineHeight = uiFont?.lineHeight ?? style.size
+        let lineSpacing = style.lineHeight - fontLineHeight
+
+        return self
+            .font(.custom(style.font.string, size: style.size).weight(style.weight))
+            // .tracking not supported
+            // .underline not supported
+            .lineSpacing(lineSpacing)
+            .padding(.vertical, lineSpacing / 2)
+    }
+}
+
 public extension View {
 
     /// Applies all aspects of a `TextStyle` to this `View`.
